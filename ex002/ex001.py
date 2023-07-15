3e4378cb50c4aa587c1b6005986f1ae98cb86b3f
sajjadsokhanvari <sajjadnetworker2015@gmail.com>
2023-07-15 16:52:25 +0330

    i do my second little project and i am happy !

---
+++ ex002/ex001.py
@@ -0,0 +1,41 @@
+a = int(input("Enter your number: "))
+
+string = ""
+a +=1
+while a > 1:
+    a -= 1
+    string += str(a)
+
+print(int(string[::-1]))
+#############3
+# print(int(''.join([str(i) for i in range(a, 0, -1)])))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
