1fd24e519f6237f5afdb2294f6e0904167fbd1a6
sajjadsokhanvari <sajjadnetworker2015@gmail.com>
2023-07-17 23:29:16 +0330

    checking to be str

---
+++ are 2str/README.md
@@ -0,0 +1,2 @@
+in this program
+we want to be str into the another str 
---
+++ are 2str/are 2str.py
@@ -0,0 +1,8 @@
+x=input("please enter your str: ")
+y=input("please enter your str: ")
+print( y in x)
+
+
+
+#heoo helo how are you i fine thank you
+#i am fine and you Mr
