4f48b82dbc88a86fbfbe05cbd1db0d35d2673b78
sajjadsokhanvari <sajjadnetworker2015@gmail.com>
2023-07-25 23:25:02 +0330

    i complete radios

--- radios/radios.py
+++ radios/radios.py
@@ -1,1 +1,7 @@
 r=float(input("enter your radios number: "))
+line=r *2
+pi=3.14
+perimeter= round(line * pi,2)
+area= round(r * r * pi,2)
+print("the number of perimeter is -->",perimeter)
+print("the number of area is ----->",area)
