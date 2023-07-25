8d72c4d8e944f663c700663d4f8c43277848cbc6
sajjadsokhanvari <sajjadnetworker2015@gmail.com>
2023-07-25 23:13:36 +0330

    i complete this programm

--- splitnumber/splitnumber.py
+++ splitnumber/splitnumber.py
@@ -1,3 +1,7 @@
 numberr=int(input("Enter your nubmerr:"))
 temp= numberr % 10   #baqi mandeh %
 print(temp)
+print("------> ",numberr)
+numberr= numberr//10   # taqsim bdon aashar
+temp= numberr % 10
+print(temp)
