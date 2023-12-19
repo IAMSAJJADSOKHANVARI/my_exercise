ae9f75a578e041c1b0f24f2c290e0a23c38bea93
sajjadsokhanvari <sajjadnetworker2015@gmail.com>
2023-12-19 20:38:16 +0330

    i write a simple programm for count str

--- count_character/count_character.py
+++ count_character/count_character.py
@@ -1,2 +1,8 @@
 string="hello how are you i'm fine thank you and you?!"
 c=(input("enter your character : "))
+count=[]
+for i in string:
+    if i == c:
+        count.append(i)
+    
+print(len(count))
